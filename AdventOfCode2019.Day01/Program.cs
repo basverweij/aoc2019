dd71f1dfd89fa86339d2bebf8ad17ed4e88e6540
Bas Verweij <basverweij@users.noreply.github.com>
2019-12-01 21:38:01 +0100

    Complete day 1, puzzle 1.

---
+++ AdventOfCode2019.Day01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day01
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle1();
+        }
+
+        static void Puzzle1()
+        {
+            Console.WriteLine(
+                "Day 01 - Puzzle 1: {0}",
+                File
+                    .ReadAllLines("input1.txt")
+                    .Select(int.Parse)
+                    .Select(GetFuelForMass)
+                    .Sum());
+        }
+
+        public static int GetFuelForMass(
+            int mass)
+        {
+            return (mass / 3) - 2;
+        }
+    }
+}
