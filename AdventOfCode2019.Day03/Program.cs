1888f461093f592bad19a9dd6de5a666dd6f90ef
Bas Verweij <basverweij@users.noreply.github.com>
2019-12-04 19:01:46 +0100

    Complete day 3, puzzle 1.

---
+++ AdventOfCode2019.Day03/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day03
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Puzzle1();
+        }
+
+        private static void Puzzle1()
+        {
+            var lines = File.ReadAllLines("input1.txt");
+
+            var line1 = ParseLine(lines[0]);
+
+            var line2 = ParseLine(lines[1]);
+
+            var points1 = DrawLine(line1);
+
+            var points2 = DrawLine(line2);
+
+            points1.IntersectWith(points2);
+
+            var closestDistance = points1
+                .Select(p => Distance(p.Item1, p.Item2))
+                .Min();
+
+            Console.WriteLine($"Day 03 - Puzzle 1: {closestDistance}");
+        }
+
+        public static Segment[] ParseLine(
+            string line)
+        {
+            return line
+                .Split(',')
+                .Select(Segment.FromString)
+                .ToArray();
+        }
+
+        public static HashSet<(int, int)> DrawLine(
+            Segment[] line)
+        {
+            var x = 0;
+
+            var y = 0;
+
+            var points = new HashSet<(int, int)>();
+
+            foreach (var segment in line)
+            {
+                if (segment.DeltaY == 0)
+                {
+                    // move along x-axis
+                    for (var i = 0; i < segment.Length; i++)
+                    {
+                        x += segment.DeltaX;
+
+                        points.Add((x, y));
+                    }
+                }
+                else
+                {
+                    // move along y-axis
+                    for (var i = 0; i < segment.Length; i++)
+                    {
+                        y += segment.DeltaY;
+
+                        points.Add((x, y));
+                    }
+                }
+            }
+
+            return points;
+        }
+
+        private static int Distance(int x, int y)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+
+            if (y < 0)
+            {
+                y = -y;
+            }
+
+            return x + y;
+        }
+    }
+}
---
+++ AdventOfCode2019.Day03/Segment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AdventOfCode2019.Day03
+{
+    public class Segment
+    {
+        public int DeltaX { get; private set; }
+
+        public int DeltaY { get; private set; }
+
+        public int Length { get; private set; }
+
+        public static Segment FromString(
+            string s)
+        {
+            var (dx, dy) = s[0] switch
+            {
+                'L' => (-1, 0),
+                'R' => (1, 0),
+                'U' => (0, 1),
+                'D' => (0, -1),
+
+                _ => throw new ArgumentException(
+                    $"invalid direction '{s[0]}': must be one of L, R, U, D",
+                    nameof(s)),
+            };
+
+            var l = int.Parse(s.Substring(1));
+
+            return new Segment { DeltaX = dx, DeltaY = dy, Length = l };
+        }
+    }
+}
